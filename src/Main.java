748fa5026e7651c9c30b9b2866003fbccfacf512
saivishalvangala <saivishalvangala@gmail.com>
2019-09-20 09:42:39 +0530

    final push with main file

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class main {
+    public static void main(String[] args)
+    {
+        System.out.println("enter 1 to run palindrome program");
+        System.out.println("enter 2 to run given conditional check program");
+        System.out.println("enter 3 to run program that checks for each single character in word whether it is vowel or consonant");
+        System.out.println("enter 4 to run a program which prints the given pattern");
+        System.out.println("enter 5 to run a program which adds all the numbers given in string format");
+        System.out.println("enter 6 to run a program that checks whether the character is capital,small or digit ");
+        System.out.println("enter 7 to run a program which sorts the digits in non-increasing order");
+        System.out.println("enter 8 to run a program which guess the user input number correctly");
+        System.out.println("enter 9 to run a program to reverse a string without string buffer");
+        System.out.println("enter 10 to run palindrome program");
+
+
+    }
+
+}
